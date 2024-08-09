3147daccec02eba91317447d3bb434796db09d70
Raphide <atlas.dino.usic@outlook.com>
2024-08-09 15:06:13 +1000

    moved methods into Board class

--- minesweeper/Board.java
+++ minesweeper/Board.java
@@ -1,131 +1,245 @@
 package minesweeper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Board {
 
-    private String header = "<==<<====<< MINESWEEPER >>===>>==>";
+    private String[][] header = {
+            { "███╗   ███╗██╗███╗   ██╗███████╗███████╗██╗    ██╗███████╗███████╗██████╗ ███████╗██████╗ " },
+            { "████╗ ████║██║████╗  ██║██╔════╝██╔════╝██║    ██║██╔════╝██╔════╝██╔══██╗██╔════╝██╔══██╗" },
+            { "██╔████╔██║██║██╔██╗ ██║█████╗  ███████╗██║ █╗ ██║█████╗  █████╗  ██████╔╝█████╗  ██████╔╝" },
+            { "██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ╚════██║██║███╗██║██╔══╝  ██╔══╝  ██╔═══╝ ██╔══╝  ██╔══██╗" },
+            { "██║ ╚═╝ ██║██║██║ ╚████║███████╗███████║╚███╔███╔╝███████╗███████╗██║     ███████╗██║  ██║" },
+            { "╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝ ╚══╝╚══╝ ╚══════╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝" }
+    };
 
     private String[][] matrix = {
-            { "+", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" },
-            { "A", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "B", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "C", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "F", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "G", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "H", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "I", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "J", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" }
+            { "+", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" },
+            { "A", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "B", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "C", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "D", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "E", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "F", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "G", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "H", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "I", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" },
+            { "J", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■" }
     };
 
-    // matrixClone "O" = mine
-    // to-do: generate random position to replace "X" with "O"
-    private String[][] matrixClone = {
-            { "+", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" },
-            { "A", "X", "X", "X", "X", "O", "X", "X", "X", "X", "O" },
-            { "B", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X" },
-            { "C", "X", "X", "O", "X", "X", "X", "X", "X", "X", "X" },
-            { "D", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X" },
-            { "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "F", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X" },
-            { "G", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X" },
-            { "H", "X", "X", "O", "O", "X", "X", "X", "X", "X", "X" },
-            { "I", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-            { "J", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X" }
+    private int[][] intMatrix = {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     };
 
-    private HashMap<Integer, Character> enteredPositions = new HashMap<>();
+    String[][] boom = {
+            { "-███████████-----███████-------███████----██████---██████-███" },
+            { "░░███░░░░░███--███░░░░░███---███░░░░░███-░░██████-██████-░███" },
+            { "-░███----░███-███-----░░███-███-----░░███-░███░█████░███-░███" },
+            { "-░██████████-░███------░███░███------░███-░███░░███-░███-░███" },
+            { "-░███░░░░░███░███------░███░███------░███-░███-░░░--░███-░███" },
+            { "-░███----░███░░███-----███-░░███-----███--░███------░███-░░░-" },
+            { "-███████████--░░░███████░---░░░███████░---█████-----█████-███" },
+            { "░░░░░░░░░░░-----░░░░░░░-------░░░░░░░----░░░░░-----░░░░░-░░░-" }
+    };
+
+    private ArrayList<String> enteredCoordinates = new ArrayList<>();
 
     public Board() {
         this.header = header;
         this.matrix = matrix;
-        this.enteredPositions = enteredPositions; // this will store positions that have already been used.
-        this.matrixClone = matrixClone; // this will be the hidden board
+        this.enteredCoordinates = enteredCoordinates; // this will store positions that have already been used.
+        this.intMatrix = intMatrix; // this will be the hidden board
+    }
+
+    public void printGameBoard() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.printf("|%s|", matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("enter a row letter between A-J");
+    }
+
+    public void createIntMatrix() {
+
+        for (int i = 0; i < intMatrix.length; i++) {
+            for (int j = 0; j < intMatrix.length; j++) {
+            }
+        }
+
+        var n = 0;
+        while (n < 10) {
+            int randomRow = (int) Math.floor(Math.random() * (10 - 0));
+            int randomColumn = (int) Math.floor(Math.random() * (10 - 0));
+            if (intMatrix[randomRow][randomColumn] != 9) {
+                intMatrix[randomRow][randomColumn] = 9;
+
+                if (randomRow < 9 && intMatrix[randomRow + 1][randomColumn] < 9) { // south
+                    intMatrix[randomRow + 1][randomColumn]++;
+                }
+                if (randomRow > 0 && intMatrix[randomRow - 1][randomColumn] < 9) { // north
+                    intMatrix[randomRow - 1][randomColumn]++;
+                }
+                if (randomRow < 9 && randomColumn < 9 && intMatrix[randomRow + 1][randomColumn + 1] < 9) { // south east
+                    intMatrix[randomRow + 1][randomColumn + 1]++;
+                }
+                if (randomRow > 0 && randomColumn > 0 && intMatrix[randomRow - 1][randomColumn - 1] < 9) { // north west
+                    intMatrix[randomRow - 1][randomColumn - 1]++;
+                }
+                if (randomRow > 0 && randomColumn < 9 && intMatrix[randomRow - 1][randomColumn + 1] < 9) { // north east
+                    intMatrix[randomRow - 1][randomColumn + 1]++;
+                }
+                if (randomRow < 9 && randomColumn > 0 && intMatrix[randomRow + 1][randomColumn - 1] < 9) { // south west
+                    intMatrix[randomRow + 1][randomColumn - 1]++;
+                }
+                if (randomColumn < 9 && intMatrix[randomRow][randomColumn + 1] < 9) { // east
+                    intMatrix[randomRow][randomColumn + 1]++;
+                }
+                if (randomColumn > 0 && intMatrix[randomRow][randomColumn - 1] < 9) { // west
+                    intMatrix[randomRow][randomColumn - 1]++;
+                }
+
+            }
+            n = n + 1;
+        }
+
+        // // not necessary to print out bomb board unless you want to check bomb
+        // // placement. Don't be a cheater :)
+        // for (int i = 0; i < intMatrix.length; i++) {
+        // for (int j = 0; j < intMatrix.length; j++) {
+        // System.out.printf("|%s|", intMatrix[i][j]);
+        // }
+
+        // System.out.println();
+
+        // }
+
     }
 
     public void setupGameBoard() {
-
-        System.out.println(header);
-
         // j = columns
         // i = rows
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.printf("|%s|", matrix[i][j]);
+        for (int i = 0; i < header.length; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.printf(header[i][j]);
             }
             System.out.println();
         }
+        System.out.println("");
+
+        createIntMatrix();
+        printGameBoard();
+
     }
 
     public void updateGameBoard() {
-        System.out.println(header);
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.printf("|%s|", matrix[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println(enteredPositions);
+        printGameBoard();
 
     }
 
-    public void takeTurn(){
-        char row;
-        int column;
+    public void takeTurn() {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a letter between A-J");
-        char rowCheck = scanner.next().toUpperCase().charAt(0);
-        if (rowCheck != 'A' || rowCheck != 'B' || rowCheck != 'C' || rowCheck !=
-        'D' || rowCheck != 'E'|| rowCheck != 'F' || rowCheck != 'G' || rowCheck !=
-        'H' || rowCheck != 'I' || rowCheck != 'J') {
-            System.out.println("please enter a valid character");
-        } 
-        row = rowCheck;
-        System.out.println("Please enter a number between 1-10");
-        column = scanner.nextInt();
+        while (scanner.hasNext()) {
 
-        System.out.printf("The row is: %c\nthe column is: %d \n", row, column);
+            var r = 1;
+            char ro = scanner.next().toUpperCase().charAt(0);
 
-        enteredPositions.put(column, row);
+            if (ro == 'A') {
+                r = 1;
+            } else if (ro == 'B') {
+                r = 2;
+            } else if (ro == 'C') {
+                r = 3;
+            } else if (ro == 'D') {
+                r = 4;
+            } else if (ro == 'E') {
+                r = 5;
+            } else if (ro == 'F') {
+                r = 6;
+            } else if (ro == 'G') {
+                r = 7;
+            } else if (ro == 'H') {
+                r = 8;
+            } else if (ro == 'I') {
+                r = 9;
+            } else if (ro == 'J') {
+                r = 10;
+            } else {
+                System.out.println("Please enter a valid character");
+                continue;
+            }
 
-        updateGameBoard();
+            System.out.println("enter a column number between 1-10");
+            int c = scanner.nextInt();
+            if (c > 10) {
+                System.out.println("Please enter a valid number");
+                System.out.println("enter a row letter between A-J");
+                continue;
+            }
 
-       
-
+            String coordinate = ro + String.valueOf(c);
+            if (enteredCoordinates.contains(coordinate)) {
+                System.out.println("This coordinate has already been revealed");
+                System.out.println("enter a row letter between A-J");
+                continue;
+            }
+            var coordValue = String.valueOf(intMatrix[r - 1][c - 1]);
+            matrix[r][c] = coordValue;
+            if (intMatrix[r - 1][c - 1] == 9) {
+                matrix[r][c] = "!";
+                for (int i = 0; i < matrix.length; i++) {
+                    for (int j = 0; j < matrix.length; j++) {
+                        System.out.printf("|%s|", matrix[i][j]);
+                    }
+                    System.out.println();
+                }
+                for (int i = 0; i < boom.length; i++) {
+                    for (int j = 0; j < 1; j++) {
+                        System.out.printf(boom[i][j]);
+                    }
+                    System.out.println();
+                }
+                System.out.println("You lose :(");
+                break;
+            } else {
+                matrix[r][c] = coordValue;
+                enteredCoordinates.add(coordinate);
+                printGameBoard();
+                hasWon();
+            }
+            if(hasWon()== false){
+                System.out.printf(coordinate + "\n");
+            System.out.println("You're safe... for now...");
+            // System.out.printf(enteredCoordinates.toString() + "\n");
+            System.out.println("enter a row letter between A-J"); 
+            } else{
+                System.out.println("YOU WIN!!!");
+                break;
+            }
+           
+        }
 
     }
 
-    public void charToInt(char rowInput) {
-        // // to convert a character to an int
-
-        // int A = 1;
-        // int B = 2;
-        // int C = 3;
-        // int D = 4;
-        // int E = 5;
-        // int F = 6;
-        // int G = 7;
-        // int H = 8;
-        // int I = 9;
-        // int J = 10;
-        // switch (rowInput) {
-        //     case 'A':
-        //         return A;
-        //         break;
-        //     case 'B':
-        //         return B;
-        // }
+    public boolean hasWon() {
+        if (enteredCoordinates.size() == 90) {
+            return true; 
+        }
+        return false;
     }
-
-    public void selectRow(char rowInput) {
-        // this function to replace an entire row with
-        // {"A","O","O","O","O","O","O","O","O","O","O"}
-        // to indicate to user that this is the row they have selected
-    }
-
 }
--- minesweeper/Main.java
+++ minesweeper/Main.java
@@ -9,55 +9,11 @@ public class Main {
         Board newGame = new Board();
 
         newGame.setupGameBoard();
-
-        //next challenge is to iterate through the board
-        // row 0 and collumn 0 should be off limits
-        //for user input the row has to be tied to a number. 0 also needs to default to 10.
        
-        while(true){
+        while(newGame.hasWon()==false){
             newGame.takeTurn();
         }
 
-        // int A = 1;
-        // int B = 2;
-        // int C = 3;
-        // int D = 4;
-        // int E = 5;
-        // int F = 6;
-        // int G = 7;
-        // int H = 8;
-        // int I = 9;
-        // int J = 10;
-
-        // char row;
-        // int column;
-
-        
-
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Please enter a letter between A-J");
-        // row = scanner.next().toUpperCase().charAt(0);
-        // System.out.println("Please enter a number between 1-10");
-        // column = scanner.nextInt();
-
-        // System.out.printf("The row is: %c\nthe column is: %d \n", row, column);
-
-        // HashMap<Character, Integer> enteredPositions = new HashMap<>();
-
-        // enteredPositions.put(row, column);
-
-        // if(enteredPositions == enteredPositions){
-        //     System.out.println("entry already exists");
-        // }
-
-        
-
-        // System.out.println(enteredPositions);
-
-        // scanner needs to be made that will take in a HashMap<Char, Integer>
-        
-
-        
     }
     
 }
